5f1bea7e246a9209e836cb69b00be3c474979c15
Erick Pulla <epulla>
2020-01-20 11:10:59 -0500

    PDUs de CalculateYearBonus() de la clase Employee

--- src/tallerpruebas/EmployeeTest.java
+++ src/tallerpruebas/EmployeeTest.java
@@ -5,26 +5,55 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 class EmployeeTest {
-
-	@Test
-	void testCs() {
-		fail("Not yet implemented");
-	}
 	
-	// Prueba con: Worker y USD
+	// Test con: Worker y USD
 	@Test
 	void testCalculateYearBonusWorkerUSD() {
-		fail("Not yet implemented");
+		Employee e = new Employee(400f, "USD", 0.48f, EmployeeType.Worker);
+		float bonus = e.CalculateYearBonus();
+		assertEquals(386f, bonus);
 	}
 	
+	// Test con: Supervisor y USD
 	@Test
 	void testCalculateYearBonusSupervisorUSD() {
-		fail("Not yet implemented");
+		Employee e = new Employee(400f, "USD", 0.48f, EmployeeType.Supervisor);
+		float bonus = e.CalculateYearBonus();
+		assertEquals(593f, bonus);
 	}
 	
+	// Test con: Manager y USD
 	@Test
 	void testCalculateYearBonusManagerUSD() {
-		fail("Not yet implemented");
+		Employee e = new Employee(400f, "USD", 0.48f, EmployeeType.Manager);
+		float bonus = e.CalculateYearBonus();
+		assertEquals(786f, bonus);
 	}
+	
+	// Test con: Worker y EUR
+	@Test
+	void testCalculateYearBonusWokerEUR() {
+		Employee e = new Employee(400f, "EUR", 0.48f, EmployeeType.Worker);
+		float bonus = e.CalculateYearBonus();
+		assertEquals(386.0f, bonus);
+	}
+	
+	// Test con: Supervisor y EUR
+	@Test
+	void testCalculateYearBonusSupervisorEUR() {
+		Employee e = new Employee(400f, "EUR", 0.48f, EmployeeType.Supervisor);
+		float bonus = e.CalculateYearBonus();
+		assertEquals(573f, bonus);
+	}
+	
+	// Test con: Manager y EUR
+	@Test
+	void testCalculateYearBonusManagerEUR() {
+		Employee e = new Employee(400f, "EUR", 0.48f, EmployeeType.Manager);
+		float bonus = e.CalculateYearBonus();
+		assertEquals(766f, bonus);
+	}
+	
+		
 
 }
