fad467eb102900e21a21e5300f54cdf6dcaccbe8
Nicole Santibanez <nsantiba@espol.edu.ec>
2020-01-20 10:47:28 -0500

    Archivos agregados

---
+++ src/tallerpruebas/EmployeeTest.java
@@ -0,0 +1,30 @@
+package tallerpruebas;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class EmployeeTest {
+
+	@Test
+	void testCs() {
+		fail("Not yet implemented");
+	}
+	
+	// Prueba con: Worker y USD
+	@Test
+	void testCalculateYearBonusWorkerUSD() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testCalculateYearBonusSupervisorUSD() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testCalculateYearBonusManagerUSD() {
+		fail("Not yet implemented");
+	}
+
+}
